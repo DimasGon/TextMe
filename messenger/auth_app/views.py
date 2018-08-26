16ec39bbcf3ddcee772ef732cbc5046bbd7d22c4
DimasGon <DimasGon@users.noreply.github.com>
2018-08-26 18:43:46 +0300

    Написание всех urls для auth_app

--- messenger/auth_app/views.py
+++ messenger/auth_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, ListView, DetailView
+from django.views.generic import View, ListView, DetailView, FormView
 from . import models
 
 class AccountView(DetailView):
@@ -7,3 +7,12 @@ class AccountView(DetailView):
     model = models.MesUser
     template_name = 'auth_app/account.html'
     context_object_name = 'account'
+
+class LogInView(FormView):
+    pass
+
+class SignInView(FormView):
+    pass
+
+class LogOutView(FormView):
+    pass
