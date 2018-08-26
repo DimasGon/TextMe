cb33d5507cce91a37c821c1045927bd8db03aa67
DimasGon <DimasGon@users.noreply.github.com>
2018-08-26 14:33:37 +0300

    Написание адресов

---
+++ messenger/auth_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('<int:pk>', views.AccountView.as_view()),
+    # path('login', views.LogInView.as_view()),
+    # path('signin', views. SignInView.as_view()),
+]
