e52618a1a6e1645c7d5e5a1058ab7df6db34cd1d
DimasGon <DimasGon@users.noreply.github.com>
2018-08-27 19:48:15 +0300

    Разработка главной страницы

--- messenger/messenger/urls.py
+++ messenger/messenger/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('account/', include('auth_app.urls')),
     path('admin/', admin.site.urls),
+    path('', include('main_app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
