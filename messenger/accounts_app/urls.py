2fba54fd6c08dbac93cd66b4601850c7a6a220fd
DimasGon <DimasGon@users.noreply.github.com>
2018-08-28 17:03:24 +0300

    Вынесение поиска в отдельную функцию

--- messenger/accounts_app/urls.py
+++ messenger/accounts_app/urls.py
@@ -4,5 +4,4 @@ from . import views
 # Путь от корня: account/
 urlpatterns = [
     path('<int:pk>', views.AccountView.as_view()),
-    path('<int:pk>', views.SearchAccountView.as_view()),
 ]
--- messenger/accounts_app/views.py
+++ messenger/accounts_app/views.py
@@ -9,48 +9,27 @@ class AccountView(LoginRequiredMixin, DetailView):
     template_name = 'accounts_app/account.html'
     context_object_name = 'account'
 
+    def search(self, context):
+        
+        search_acc = self.request.GET.get('q')
+            
+        try:
+            search_acc = models.MesUser.objects.get(username=search_acc)
+        except models.MesUser.DoesNotExist:
+            search_acc = None
+
+        if search_acc and search_acc != self.request.user:
+            context['search_acc'] = search_acc
+        else:
+            context['search_acc'] = 'Нет совпадений'
+
+        return context
+
     def get_context_data(self, **kwargs):
 
         context = super().get_context_data(**kwargs)
 
-        search_acc = self.request.GET.get('q')
-
-        if search_acc is not None:
-            
-            try:
-                search_acc = models.MesUser.objects.get(username=search_acc)
-            except models.MesUser.DoesNotExist:
-                search_acc = None
-
-            if search_acc:
-                context['search_acc'] = search_acc  
-
-            else:
-                context['search_acc'] = 'Нет совпадений'
+        if self.request.GET.get('q'):
+            context = self.search(context)
 
         return context
-
-class SearchAccountView(LoginRequiredMixin, DetailView):
-
-    model = models.MesUser
-    template_name = 'accounts_app/account.html'
-    context_object_name = 'account'
-
-    # def get(self, request, pk):
-
-    #     search_acc = request.GET.get('q')
-
-    #     if search_acc is not None:
-            
-    #         context = self.get_context_data()
-    #         search_acc = models.MesUser.objects.get(username=search_acc)
-
-    #         if search_acc:
-
-    #             context['search_acc'] = search_acc
-
-    #             return render(request, self.template_name, context)
-
-    #         return render(request, self.template_name, context)            
-
-    #     return super(SearchAccountView, self).get(request)
