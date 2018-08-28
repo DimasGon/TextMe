f2a06d7d1253322c58002823a7e81707a232b7f0
DimasGon <DimasGon@users.noreply.github.com>
2018-08-28 20:39:44 +0300

    Удаление сигналов из модели пользователя

--- messenger/auth_app/models.py
+++ messenger/auth_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-from django.db.models.signals import pre_save, post_save
 
 class MesUser(AbstractUser):
 
