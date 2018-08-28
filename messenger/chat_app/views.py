9ca1de426fbbd39a4f2c5b15b767d8c033c59e5d
DimasGon <DimasGon@users.noreply.github.com>
2018-08-28 13:06:01 +0300

    Разработка контроллера и шаблона для пустого окна чатов

--- messenger/chat_app/views.py
+++ messenger/chat_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class EmptyChatView(TemplateView):
+    template_name = 'chat_app/chat.html'
