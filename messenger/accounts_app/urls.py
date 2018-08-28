8ff86e1eb8a8929a423144b4299b6d1ed9a49c1a
DimasGon <DimasGon@users.noreply.github.com>
2018-08-28 16:52:32 +0300

    Реализация поиска друзей

--- messenger/accounts_app/urls.py
+++ messenger/accounts_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 # Путь от корня: account/
 urlpatterns = [
     path('<int:pk>', views.AccountView.as_view()),
+    path('<int:pk>', views.SearchAccountView.as_view()),
 ]
--- messenger/accounts_app/views.py
+++ messenger/accounts_app/views.py
@@ -8,3 +8,49 @@ class AccountView(LoginRequiredMixin, DetailView):
     model = models.MesUser
     template_name = 'accounts_app/account.html'
     context_object_name = 'account'
+
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+
+        search_acc = self.request.GET.get('q')
+
+        if search_acc is not None:
+            
+            try:
+                search_acc = models.MesUser.objects.get(username=search_acc)
+            except models.MesUser.DoesNotExist:
+                search_acc = None
+
+            if search_acc:
+                context['search_acc'] = search_acc  
+
+            else:
+                context['search_acc'] = 'Нет совпадений'
+
+        return context
+
+class SearchAccountView(LoginRequiredMixin, DetailView):
+
+    model = models.MesUser
+    template_name = 'accounts_app/account.html'
+    context_object_name = 'account'
+
+    # def get(self, request, pk):
+
+    #     search_acc = request.GET.get('q')
+
+    #     if search_acc is not None:
+            
+    #         context = self.get_context_data()
+    #         search_acc = models.MesUser.objects.get(username=search_acc)
+
+    #         if search_acc:
+
+    #             context['search_acc'] = search_acc
+
+    #             return render(request, self.template_name, context)
+
+    #         return render(request, self.template_name, context)            
+
+    #     return super(SearchAccountView, self).get(request)
