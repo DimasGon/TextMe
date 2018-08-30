bfe79015b1147a4a8152ced86c30d7ac6b72083a
DimasGon <DimasGon@users.noreply.github.com>
2018-08-30 17:51:40 +0300

    Разработка создания и отображения постов на стене и комментов

--- messenger/accounts_app/views.py
+++ messenger/accounts_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from django.views.generic import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
@@ -9,7 +9,7 @@ class AccountView(LoginRequiredMixin, DetailView):
     template_name = 'accounts_app/account.html'
     context_object_name = 'account'
 
-    def search(self, context):
+    def get_search(self, context):
 
         search_acc = self.request.GET.get('q')
 
@@ -25,19 +25,40 @@ class AccountView(LoginRequiredMixin, DetailView):
 
         return context
 
+    def post_wallpost(self, account_id):
+        
+        author = self.request.user
+        account = models.AccountModel.objects.get(id=account_id)
+        post_text = self.request.POST.get('wallpost')
+
+        models.WallPostModel(author=author, account=account, post_text=post_text).save()
+
+    def post_comment(self):
+        
+        author = self.request.user
+        comment_text = self.request.POST.get('comment')
+        to_post = int(self.request.POST.get('to_post'))
+        post = models.WallPostModel.objects.get(id=to_post)
+
+        models.CommentModel(author=author, comment_text=comment_text, to_post=post).save()
+
     def get_context_data(self, **kwargs):
 
         context = super().get_context_data(**kwargs)
         account = context['account']
         context['wallposts'] = account.get_wallposts()
-
+        
         if self.request.GET.get('q'):
-            context = self.search(context)
+            context = self.get_search(context)
 
         return context
 
-    def post(self, request):
+    def post(self, request, pk):
 
-        # if request.POST.get('wallpost')
+        if request.POST.get('wallpost'):
+            self.post_wallpost(pk)
 
-        return super().post(request)
+        elif request.POST.get('comment'):
+            self.post_comment()
+
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
