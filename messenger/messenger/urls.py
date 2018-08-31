796c561af1608e6c0751956e07a293290267dadf
DimasGon <DimasGon@users.noreply.github.com>
2018-08-31 13:52:27 +0300

    Изменение url chats на chat

--- messenger/messenger/urls.py
+++ messenger/messenger/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts_app.urls')),
-    path('chats/', include('chat_app.urls')),
+    path('chat/', include('chat_app.urls')),
     path('', include('auth_app.urls')),
     path('', include('main_app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
