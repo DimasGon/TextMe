56942135f03e5236079e34b403a6922c3136a33b
DimasGon <DimasGon@users.noreply.github.com>
2018-08-31 17:09:35 +0300

    Написание адресов чата

--- messenger/chat_app/urls.py
+++ messenger/chat_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.EmptyChatView.as_view()),
-    # path('<int:pk>', views.ChatView.as_view())
+    path('start_messaging/<int:partner_id>', views.StartChatView.as_view()),
+    path('<int:pk>', views.ChatView.as_view()),
 ]
