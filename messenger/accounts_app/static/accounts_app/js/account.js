86eb073280b3ef71dfae025721d63b73a5cf64f2
DimasGon <DimasGon@users.noreply.github.com>
2018-09-05 18:42:23 +0300

    Оптимизация кода accounts.js

--- messenger/accounts_app/static/accounts_app/js/account.js
+++ messenger/accounts_app/static/accounts_app/js/account.js
@@ -1,3 +1,30 @@
+var reloadAccPage = function () { //
+    var form = $(this);
+    $.ajax({
+        url: form.attr("action"),
+        data: form.serialize(),
+        type: form.attr("method"),
+        dataType: 'json',
+        success: function (data) {
+            $("#js-insert-acc").html(data.html_page);
+        }
+    });
+    return false;
+}
+var reloadBookmarksPage = function () { ////
+    var form = $(this);
+    $.ajax({
+        url: form.attr("action"),
+        data: form.serialize(),
+        type: form.attr("method"),
+        dataType: 'json',
+        success: function (data) {
+            $("#js-insert-bookmarks").html(data.html_page);
+        }
+    });
+    return false;
+}
+
 $(function () {
 
     $.ajax({
@@ -17,61 +44,11 @@ $(function () {
             $("#js-insert-bookmarks").html(data.html_page);
         }
     });
-  
-});
 
-$("#js-insert-acc").on("submit", ".js-add-post", function () {
-    var form = $(this);
-    $.ajax({
-        url: form.attr("action"),
-        data: form.serialize(),
-        type: form.attr("method"),
-        dataType: 'json',
-        success: function (data) {
-            $("#js-insert-acc").html(data.html_page);
-        }
-    });
-    return false;
-});
+    $("#js-insert-acc").on("submit", ".js-add-post", reloadAccPage);
+    $("#js-insert-acc").on("submit", ".js-add-comment", reloadAccPage);
 
-$("#js-insert-acc").on("submit", ".js-add-comment", function () {
-    var form = $(this);
-    $.ajax({
-        url: form.attr("action"),
-        data: form.serialize(),
-        type: form.attr("method"),
-        dataType: 'json',
-        success: function (data) {
-            $("#js-insert-acc").html(data.html_page);
-        }
-    });
-    return false;
-});
+    $("#js-insert-acc").on("submit", ".js-add-bookmarks", reloadBookmarksPage);
+    $("#js-insert-bookmarks").on("submit", ".js-search-bookmarks", reloadBookmarksPage);
 
-$("#js-insert-acc").on("submit", ".js-add-bookmarks", function () {
-    var form = $(this);
-    $.ajax({
-        url: form.attr("action"),
-        data: form.serialize(),
-        type: form.attr("method"),
-        dataType: 'json',
-        success: function (data) {
-            $("#js-insert-bookmarks").html(data.html_page);
-        }
-    });
-    return false;
-});
-
-$("#js-insert-bookmarks").on("submit", ".js-add-bookmarks", function () {
-    var form = $(this);
-    $.ajax({
-        url: form.attr("action"),
-        data: form.serialize(),
-        type: form.attr("method"),
-        dataType: 'json',
-        success: function (data) {
-            $("#js-insert-bookmarks").html(data.html_page);
-        }
-    });
-    return false;
 });
